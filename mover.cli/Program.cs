916d69ad1bb085a3a1458e1cd8d5a44e7ac129c5
pierre@audrey <nop77svk@gmail.com>
2023-09-09 19:41:30 +0200

    ➕ CLI mover now works 🙂

--- mover.cli/Program.cs
+++ mover.cli/Program.cs
@@ -1,9 +1,16 @@
 namespace NoP77svk.Mover.Cli;
+using System;
+using NoP77svk.Mover.Core;
 
 internal static class Program
 {
-    internal static int Main(string[] args)
+    internal static async Task<int> Main(string[] args)
     {
-        return 0;
+        IMouseCursorMover mover = new RelativeMouseCursorMover(5);
+        while (true)
+        {
+            mover.NextMove();
+            await Task.Delay(10);
+        }
     }
 }
