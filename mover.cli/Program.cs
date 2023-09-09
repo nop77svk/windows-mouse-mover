ec587a5af7c8254c5ed4fe65cc26b7e968f29184
pierre@audrey <nop77svk@gmail.com>
2023-09-09 19:44:11 +0200

    🧹

--- mover.cli/Program.cs
+++ mover.cli/Program.cs
@@ -4,7 +4,7 @@ using NoP77svk.Mover.Core;
 
 internal static class Program
 {
-    internal static async Task<int> Main(string[] args)
+    internal static async Task Main(string[] args)
     {
         IMouseCursorMover mover = new RelativeMouseCursorMover(5);
         while (true)
