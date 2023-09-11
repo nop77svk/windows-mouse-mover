c9690e63878a83a4bb3119adff812869d6003e24
Peter Hraško <peter_hrasko@ri-rpc.sk>
2023-09-11 14:58:23 +0200

    ➕ safeguarded against non-Windows platforms

--- mover.core/MouseCursorPosition.cs
+++ mover.core/MouseCursorPosition.cs
@@ -5,6 +5,9 @@ public static class MouseCursorPosition
 {
     public static void GetAbsolute(out int x, out int y)
     {
+        if (Environment.OSVersion.Platform is not PlatformID.Win32NT)
+            throw new NotSupportedException($"We don't (yet) support \"${Environment.OSVersion.Platform.ToString()}");
+
         WindowsPoint result = new WindowsPoint() { X = -1, Y = -1 };
         if (!WindowsDllUser32.GetCursorPos(ref result))
             throw new Exception("Failed to obtain mouse cursor position via user32.dll call");
@@ -15,6 +18,9 @@ public static class MouseCursorPosition
 
     public static void SetAbsolute(int x, int y)
     {
+        if (Environment.OSVersion.Platform is not PlatformID.Win32NT)
+            throw new NotSupportedException($"We don't (yet) support \"${Environment.OSVersion.Platform.ToString()}");
+
         if (!WindowsDllUser32.SetCursorPos(x, y))
             throw new Exception("Failed to set mouse cursor position via user32.dll call");
     }
